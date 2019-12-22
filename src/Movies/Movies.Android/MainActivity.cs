5801689f13987851089c06c1cf74b1e8e61c6dcc
minhhieu1612 <hieunguyenminh562@gmail.com>
2019-12-22 22:05:24 +0700

    initial workflow

---
+++ src/Movies/Movies.Android/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using FFImageLoading.Forms.Platform;
+namespace Movies.Droid
+{
+    [Activity(
+        Label = "Movies", 
+        Icon = "@drawable/icon", 
+        Theme = "@style/MainTheme",
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            CachedImageRenderer.Init(enableFastRenderer: true);
+            Xamarin.FormsMaps.Init(this, bundle);
+            LoadApplication(new Movies.App());
+        }
+    }
+}
